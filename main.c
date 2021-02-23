d6b65d9de1d7a9f6b9b0c2a9019675db3c45f790
William Denning <denning@student.pcsstn.net>
2021-02-23 14:37:39 +0000

    added recursion

--- main.c
+++ main.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
 
-int main(void) {
+void add_digits(int input, int output)
+{
+  int mod;
+  
+  mod = input % 10;
+  output = output + mod;
+  input = input / 10;
 
+  if (input >= 1)
+  {
+    add_digits(input, output);
+  }
+  else
+  {
+    printf("%i", output);
+  }
+}
+
+int main(void) 
+{
   int input;
   int output = 0;
-  int mod;
 
   printf("number?\n");
   scanf("%i", &input);
 
-  do 
-  {
-    mod = input % 10;
-    output = output + mod;
-    input = input / 10;
-  } while (input >= 1);
-
-  printf("%i", output);
+  add_digits(input, output);
 
   return 0;
 }
